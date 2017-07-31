f7063d1b7f4ba9f0eed96713afe4db77e9f543ee
Sasha <sasha310209@gmail.com>
2017-07-31 22:08:24 +0300

    changed directory

---
+++ src/js/common.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    /*
+    * $1 menu js file
+    * $2 fixed footer js file
+    * $3 fixed elements picture and scrolled content page
+    * $4 sliders file import
+     *$5 popUp info script
+    * */
+
+});
